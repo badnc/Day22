d45cf470e7a5216ad6cb20879ffdddf35fce63e0
badnc <b.dunic@hotmail.com>
2021-03-30 10:19:29 +0200

    React

--- ex10/src/Box.js
+++ ex10/src/Box.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 
 function Box(props) {
-return <h2>This is a {this.props.size}px Box!</h2>
+    return <h2>This is a {props.size}px Box!</h2>
 }
 
-class Box extends Component {
-    render() {
-        return (
-            // <h2>This is a {this.props.size}px Box!</h2>
-            <Box />
-        )
-    }
-}
+// class Box extends Component {
+//     render() {
+//         return (
+//             // <h2>This is a {this.props.size}px Box!</h2>
+//             <BoxBox />
+//         )
+//     }
+// }
 
 export default Box;
